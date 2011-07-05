27c3071dbbbbf17525d89d9d4b45b7d57db2caad
mackrell <dmackrell@me.com>
2011-07-05 19:51:58 +0800

    Changed headers

--- Vermont Recipes/main.m
+++ Vermont Recipes/main.m
@@ -1,9 +1,9 @@
 //
 //  main.m
-//  Vermont Recipes
+//  Vermont Recipes 2.0.0
 //
 //  Created by Derek Mackrell on 7/3/11.
-//  Copyright 2011 Cengage Learning. All rights reserved.
+//  Copyright © Bill Cheeseman 2000-2009. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
